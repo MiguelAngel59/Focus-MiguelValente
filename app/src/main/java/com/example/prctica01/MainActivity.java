38a47391e838c5298930167c7b658bc3902abd79
Erick Gael García Gutiérrez <148516237+erick6aelgg@users.noreply.github.com>
2026-02-24 00:16:35 -0600

    feat: add @author in MainActivity.java

--- app/src/main/java/com/example/prctica01/MainActivity.java
+++ app/src/main/java/com/example/prctica01/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+/* *
+ * @author <a href=erick6aelgg@ciencias.unam.mx> Erick Gael García Gutiérrez - @erick6aelgg </>
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
