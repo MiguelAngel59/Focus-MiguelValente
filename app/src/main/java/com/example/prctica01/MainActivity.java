c92971b76dd9404134c86586baf2de739643a360
MiguelAngel59 <miguel.mavt59@gmail.com>
2026-03-10 16:27:13 -0600

    feat: add lifeCicle methods

--- app/src/main/java/com/example/prctica01/MainActivity.java
+++ app/src/main/java/com/example/prctica01/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.util.Log;
 
 /* *
  * @author <a href="mailto:miguel.mavt59@ciencias.unam.mx">Miguel Angel Valente Trinidad - @MiguelAngel59</a>
@@ -16,15 +17,44 @@ import androidx.core.view.WindowInsetsCompat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "LifecycleTest" ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        Log . d ( TAG , " onCreate: ejecutado " ) ;
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    protected void onStart () {
+        super . onStart () ;
+        Log . d ( TAG , " onStart: ejecutado " ) ;
+    }
+    @Override
+    protected void onResume () {
+        super . onResume () ;
+        Log . d ( TAG , " onResume: ejecutado " ) ;
+    }
+    @Override
+    protected void onPause () {
+        super . onPause () ;
+        Log . d ( TAG , " onPause: ejecutado " ) ;
+    }
+    @Override
+    protected void onStop () {
+        super . onStop () ;
+        Log . d ( TAG , " onStop: ejecutado " ) ;
+    }
+    @Override
+    protected void onDestroy () {
+        super . onDestroy () ;
+        Log . d ( TAG , " onDestroy: ejecutado " ) ;
     }
 }
